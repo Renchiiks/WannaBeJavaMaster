bec14841738bda6c7af4417698d6aa11f61a4f2c
Renchiiks <60191731+Renchiiks@users.noreply.github.com>
2020-02-19 21:20:36 +0200

    Update JavaMaster.java

--- Wanna be Java Master/src/JavaMaster/JavaMaster.java
+++ Wanna be Java Master/src/JavaMaster/JavaMaster.java
@@ -1,11 +1,13 @@
-package JavaMaster;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class JavaMaster {
 
 	public static void main(String[] args) {
+
+		int points = 0;
+		int points2 = 0;
+
 		Questions[] allQuestions = GetQuestions();
 
 		Scanner sc = new Scanner(System.in);
@@ -14,30 +16,62 @@ public class JavaMaster {
 			System.out.println(questions.question);
 			System.out.println(Arrays.toString(questions.options));
 			int atbilde = sc.nextInt();
-			if (atbilde == questions.answer) {
-				System.out.println("nais");
+			
+			String regex = "^([1-4])";
+			String data = Integer.toString(atbilde);
+			
 
-			}else {
-				System.out.println("Nope");
+			while (atbilde != questions.answer ) {
+				if (!(data.matches(regex))){
+					System.out.println("Enter 1-4");
+					
+
+				System.out.println("Try again! Tip: " + questions.tip);
+				points += 2;
+				atbilde = sc.nextInt();
+				}
 			}
-
+			System.out.println("nais");
+			points2 += 10;
+		
 		}
-
+		int sum = points2 - points;
+		System.out.println("You earned: " + sum + " points!");
+		System.out.println("See you in next level!");
 	}
 
 	private static Questions[] GetQuestions() {
-		Questions[] returnArray = new Questions[2];
+		Questions[] returnArray = new Questions[10];
+		Options options = new Options();
 
-		String[] options1 = { "1. Java is a programming language, created in 1995",
-				"\n2. Java is a popular programming language, created in 1992",
-				"\n3. Java is one of the less famous and rarely used programming languages",
-				"\n4. Java is disgusting food" };
-		String[] options2 = { "1.Java Development Kit", "\n2. Java Drive Kit", "\n3. Java Development Keyboard",
-				"\n4. Journey Development Kit" };
-		returnArray[0] = new Questions("What is Java?", options1, 1);
-		returnArray[1] = new Questions("What is JDK?", options2, 1);
+		returnArray[0] = new Questions("What is Java?", options.options1, 1,
+				Tips.tip1);
+		returnArray[1] = new Questions("What is JDK?", options.options2, 1,
+				Tips.tip2);
+		returnArray[2] = new Questions(
+				"Which one isn't access modifier in Java?", options.options3,
+				4, Tips.tip3);
+		returnArray[3] = new Questions("When arrays are used?",
+				options.options4, 4, Tips.tip4);
+		returnArray[4] = new Questions(
+				"What you will see on console when enter: System.out.println(null)?",
+				options.options5, 4, Tips.tip5);
+		returnArray[5] = new Questions(
+				"Why Java program needs Java main method?", options.options6,
+				1, Tips.tip6);
+		returnArray[6] = new Questions("Why we use package in Java?",
+				options.options7, 4, Tips.tip7);
+		returnArray[7] = new Questions(
+				"Some Java Reserved Keywords are: abstract, boolean, break, case, class, enum, native, private, strictfp, super, throw, throne, void, while. Which word is wrong (not Keyword)?",
+				options.options8, 4, Tips.tip8);
+		returnArray[8] = new Questions(
+				"Which fact about the Constructor is wrong?", options.options9,
+				2, Tips.tip9);
+		returnArray[9] = new Questions("Why isn't Java 100% Object-oriented?",
+				options.options10, 3, Tips.tip10);
 
 		return returnArray;
+
 	}
 
 }
